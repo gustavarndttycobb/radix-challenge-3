921d78817bcde863dec826cb3983f8a9b81e7e6c
gustavoarndttycobb <gustavo.tycobb@gmail.com>
2025-06-19 17:17:19 -0300

    feat:create request to an endpoint

--- fakeSensors/main.py
+++ fakeSensors/main.py
@@ -1,14 +1,19 @@
 import time
 import json
-from datetime import datetime
 import random
+from datetime import datetime, timedelta, timezone
+import requests
 
 EQUIPMENT_ID = "EQ-12495"
+API_URL = "https://httpbin.org/post"  # Pode substituir por sua URL real
+
+# Forçar timezone -05:00
+tz_offset = timezone(timedelta(hours=-5))
 
 def generate_data():
     return {
         "equipmentId": EQUIPMENT_ID,
-        "timestamp": datetime.now().astimezone().isoformat(),
+        "timestamp": datetime.now(tz=tz_offset).isoformat(),
         "value": round(random.uniform(50.0, 100.0), 2)
     }
 
@@ -16,7 +21,12 @@ if __name__ == "__main__":
     try:
         while True:
             data = generate_data()
-            print(json.dumps(data))
+            print("Enviando:", data)
+            
+            response = requests.post(API_URL, json=data)
+            print("Status:", response.status_code)
+            print("Resposta:", response.json())
+
             time.sleep(1)
     except KeyboardInterrupt:
         print("\nEncerrado pelo usuário.")
